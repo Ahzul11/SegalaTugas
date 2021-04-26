16f19c817b7743a0224cc2daa5ba3109f72b23c4
Ahzul11 <>
2021-04-26 10:09:25 +0000

    tugas bu betty

---
+++ LATIHAN_PYTHON/__init__.py
@@ -0,0 +1,5 @@
+import os
+
+os.chdir(r"https://replit.com/@Ahzul11/SegalaTugas#LATIHAN_PYTHON/")
+
+os.getcwd()
---
+++ main.py
@@ -0,0 +1,6 @@
+import os
+
+os.chdir(r"https://replit.com/@Ahzul11/SegalaTugas/LATIHAN_PYTHON/")
+import latihan_package.alpha as a, latihan_package.beta as b
+
+os.getcwd()
